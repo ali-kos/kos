3f1a1cffad4e6621895562352b757a1454a20116
杨晓哲 <yxz@choicesoft.com.cn>
2018-07-26 14:12:16 +0800

    增加对Redux DevTools插件的支持

--- src/store.js
+++ src/store.js
@@ -2,7 +2,7 @@
 import Model from './model';
 import asyncMiddleware from './async-middleware';
 import RootReducer from './root-reducer';
-import { createStore, applyMiddleware } from 'redux';
+import { createStore, applyMiddleware, compose } from 'redux';
 
 
 
@@ -17,7 +17,12 @@ export default (middlewareList) => {
     const initial = Model.getInitial();
     const rootReducer = RootReducer(Model);
 
-    window.store = store = createStore(rootReducer, initial, enchance); // eslint-disable-line
+    if (!window.__REDUX_DEVTOOLS_EXTENSION__) {
+      window.store = store = createStore(rootReducer, initial, enchance); // eslint-disable-line
+    } else {
+      window.store = store = createStore(rootReducer, initial, // eslint-disable-line
+        compose(enchance, window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__())); // eslint-disable-line
+    }
   }
 
   return store
